cd21b4f906a8eadcb5a5cb36ae0ccad2512ba360
Eric-Paul Lecluse <e@epologee.com>
2013-01-31 17:49:05 +0100

    Initial commit

---
+++ Objective-C Unit Test Bundle.xctemplate/___PACKAGENAMEASIDENTIFIER___.m
@@ -0,0 +1,32 @@
+//
+//  ___FILENAME___
+//  ___PACKAGENAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#import "___FILEBASENAME___.h"
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ___PACKAGENAME___");
+}
+
+@end
